915367cfbfeb5653df098836745b5dc4ee3d5fc2
Emre Can Dereköy <emrecanderekoy@gmail.com>
2018-04-02 00:27:53 +0300

    2.04.2014

--- counter/app/src/main/java/com/example/jd_em/counter/MainActivity.java
+++ counter/app/src/main/java/com/example/jd_em/counter/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.jd_em.counter;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,64 @@ public class MainActivity extends AppCompatActivity {
 
         initCompenents();
         registerEventHandlers();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.ana_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.cikis:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Emin Misiniz?");
+                builder.setMessage("Sistemden çıkmak istediğinize emin misiniz?");
+
+               /* builder.setPositiveButton("Çık", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Çıkış Gerçekleşti", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                builder.setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "İptal Edildi", Toast.LENGTH_SHORT).show();
+
+                    }
+                });  */
+
+                class AlertDialogClickListener implements DialogInterface.OnClickListener{
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == AlertDialog.BUTTON_NEGATIVE){
+                            Toast.makeText(MainActivity.this,"Çıkış başarıyla gerçekleşti.",Toast.LENGTH_SHORT).show();
+                        } else if (which == AlertDialog.BUTTON_POSITIVE){
+                            Toast.makeText(MainActivity.this,"İşlem İptal Edildi.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+
+                AlertDialogClickListener alertDialogClickListener = new AlertDialogClickListener();
+                builder.setNegativeButton("Evet", alertDialogClickListener);
+                builder.setPositiveButton("Hayır", alertDialogClickListener);
+
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+
+                break;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     private void initCompenents(){
